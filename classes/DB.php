5265f7e82b3416763c54dc373806cfe41dd309aa
hasandip <hasankhandip@gmail.com>
2025-04-29 21:02:49 +0600

    test commit

---
+++ classes/DB.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'config.php';
+
+class DB {
+    private static $pdo;
+    public static function Connection() {
+        if (! isset(self::$pdo)) {
+            try {
+                self::$pdo = new PDO('mysql:host=' . DB_NAME . '; dbname=' . DB_NAME, DB_USER, DB_PASS);
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+    }
+}
+?>
